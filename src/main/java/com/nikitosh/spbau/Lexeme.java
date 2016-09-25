1f78260554b3ad705a69d4785b872a5a83ed76c1
Nikitosh <npodguzov@yandex.ru>
2016-09-26 00:15:22 +0300

    Add Lexeme class

---
+++ src/main/java/com/nikitosh/spbau/Lexeme.java
@@ -0,0 +1,44 @@
+package com.nikitosh.spbau;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Lexeme {
+    public enum Type {
+        TEXT,
+        SPACE,
+        PIPE,
+        SINGLE_QUOTE,
+        DOUBLE_QUOTE,
+        SUBSTITUTION,
+        ASSIGNMENT
+    }
+
+    private static final Map<Character, Type> TYPES = new HashMap<Character, Type>() {{
+        put(' ',  Type.SPACE);
+        put('|',  Type.PIPE);
+        put('\'', Type.SINGLE_QUOTE);
+        put('"',  Type.DOUBLE_QUOTE);
+    }};
+
+
+    private Type type;
+    private String content;
+
+    Lexeme(Type type, String content) {
+        this.type = type;
+        this.content = content;
+    }
+
+    Type getType() {
+        return type;
+    }
+
+    String getContent() {
+        return content;
+    }
+
+    public static Type getType(char symbol) {
+        return TYPES.get(symbol);
+    }
+}
