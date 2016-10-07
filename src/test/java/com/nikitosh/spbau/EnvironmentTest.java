75694257c0b6339251c1f9730e66b960ee7ef789
Nikitosh <npodguzov@yandex.ru>
2016-10-07 20:14:17 +0300

    Change assertEquals() args to order (expected, actual)

--- src/test/java/com/nikitosh/spbau/EnvironmentTest.java
+++ src/test/java/com/nikitosh/spbau/EnvironmentTest.java
@@ -11,15 +11,15 @@ public class EnvironmentTest {
     @Test
     public void testNoKey() throws IOException {
         Environment environment = new EnvironmentImpl();
-        assertEquals(environment.getValue("variable"), "");
+        assertEquals("", environment.getValue("variable"));
     }
 
     @Test
     public void testGetKey() throws IOException {
         Environment environment = new EnvironmentImpl();
         environment.setValue("variable", "value1");
-        assertEquals(environment.getValue("variable"), "value1");
+        assertEquals("value1", environment.getValue("variable"));
         environment.setValue("variable", "value2");
-        assertEquals(environment.getValue("variable"), "value2");
+        assertEquals("value2", environment.getValue("variable"));
     }
 }
